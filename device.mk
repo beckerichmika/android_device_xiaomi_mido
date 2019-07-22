1e9e5f9a10a6d15b61f44ebbb274e61c588cf63f
Mika Beckerich <beckerichmika@gmail.com>
2019-07-22 14:08:35 +0200

    Adding the udev rules to the ubuntu overlay
    Change-Id: I27fbf87588f4d2cf658399745175abe112917bb7

--- device.mk
+++ device.mk
@@ -280,8 +280,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/ubuntu/init_setup_first_boot.sh:system/halium/var/lib/lxc/android/pre-start.d/init_setup_first_boot.sh \
     $(LOCAL_PATH)/ubuntu/init_hcismd_up.sh:system/halium/var/lib/lxc/android/pre-start.d/init_hcismd_up.sh \
+    $(LOCAL_PATH)/ubuntu/70-android.rules:system/halium/lib/udev/rules.d/70-android.rules
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES  += \
      persist.camera.shutter.disable=1 \
-     camera.disable_zsl_mode=1
+     camera.disable_zsl_mode=1 \
      ro.lirc.dev=/dev/peel_ir
